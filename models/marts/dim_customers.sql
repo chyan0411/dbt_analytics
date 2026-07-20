5662d8a9c68ca364f516a7928e9015b5791fe5fe
chaoyan <chao.yan@rave.aero>
2026-07-20 14:24:46 +0000

    added src yaml file in staging folder. added source in customers and orders staging sqls

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,12 +1,12 @@
 with customers as (
 
-select * from {{ref('stg_jaffel_shop__customers')}}
+select * from {{ref('stg_jaffle_shop__customers')}}
 
 ),
 
 orders as (
 
-select * from {{ref('stg_jaffel_shop__orders')}}
+select * from {{ref('stg_jaffle_shop__orders')}}
 
 ),
 
