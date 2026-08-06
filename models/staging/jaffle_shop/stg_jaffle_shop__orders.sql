faa74cb2e6ed0a44c8cbfa7b3adeeb8d05c93011
Chao Yan <chao.yan@rave.aero>
2026-08-06 14:52:16 +0000

    modified the orders query and practiced on unit test. adding two unit tests: test_days_since_ordered and test_order_status_clean_up

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -1,7 +1,34 @@
-    select
+  {#  select
         id as order_id,
         user_id as customer_id,
         order_date,
         status as order_status
 
     from {{ source('jaffle_shop', 'orders') }}
+#}
+
+
+with 
+
+source as (
+    select * from {{ source('jaffle_shop', 'orders') }}
+),
+
+staged as (
+
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        datediff( 'day', order_date, {{ dbt.current_timestamp() }} ) as day_since_ordered,
+        status like '%pending' as is_status_pending,
+        case
+            when status like '%return%'  then 'returned' 
+            when status like '%pending%' then 'placed'
+            else status
+        end as order_status,
+    from source
+
+)
+
+select * from staged
